ed04643fb1ffb7c95c3a7bb21d0256e0ae877d4d
Ricardo Silva <ricardoslv688@gmail.com>
2020-06-02 18:18:12 +0100

    criaçao das migraçoes e geraçao de seeds

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -5,8 +5,9 @@ import path from 'path';
 const connection = knex({
   client: 'sqlite3',
   connection: {
-    filename: path.resolve(__dirname, 'database.sqlite'),
+    filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
   },
+  useNullAsDefault: true,
 });
 
 export default connection;
